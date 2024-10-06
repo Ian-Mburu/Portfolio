a5f2999c986f4d67e4fb32bcec885660066481df
Ian-Mburu <ianongaruiya@gmail.com>
2024-10-06 20:02:34 +0300

    8

--- project/settings.py
+++ project/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-^%vfxcaw)649t3f75ay(u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ '127.0.0.1', 'localhost']
-
+# 
 # Application definition
 
 INSTALLED_APPS = [
