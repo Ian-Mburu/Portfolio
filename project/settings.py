22d4755bc3e98ffa7cb1ba1b1d580a64d1ccf87c
Ian-Mburu <ianongaruiya@gmail.com>
2025-04-10 16:27:50 +0300

    connmax

--- project/settings.py
+++ project/settings.py
@@ -88,7 +88,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600 
     )
 }
 
