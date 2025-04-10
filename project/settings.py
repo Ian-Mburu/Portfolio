3b43bd6ee5a69c136dc453a0baa9eedca4eedec3
Ian-Mburu <ianongaruiya@gmail.com>
2025-04-10 15:48:44 +0300

    config

--- project/settings.py
+++ project/settings.py
@@ -87,7 +87,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.parse(config("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 # DATABASES = {
