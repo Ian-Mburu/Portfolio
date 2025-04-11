7d40b8abdb8bc636e774f197fbb894c6d760fac1
Ian-Mburu <ianongaruiya@gmail.com>
2025-04-11 23:00:43 +0300

    db

--- project/settings.py
+++ project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': dj_database_url.config(
         conn_max_age=600,
         conn_health_checks=True,
-        ssl_require=True  # Force SSL
+        ssl_require=not DEBUG  # Force SSL
     )
 }
 
