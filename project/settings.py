20f0455577b58f9de853846ee967c430e5641551
Ian-Mburu <ianongaruiya@gmail.com>
2025-04-11 22:50:03 +0300

    ssl connection

--- project/settings.py
+++ project/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
